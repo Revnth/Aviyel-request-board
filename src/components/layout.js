928f6ade64434ea35cc01afd6ede33298cc07e7c
Revanth <revanth123.rp40@gmail.com>
2022-04-05 00:08:40 +0530

    minor changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,12 @@
 import "./layout.css";
 import logo from "../assets/images/logo.png";
-import bgimg from "../assets/images/background.png";
-import line from "../assets/images/line.png";
+
 
 const Layout = () => {
   return (
     <>
       <div className="wrapper">
-        <img className="bg" src={bgimg} alt="logo" />
+        
 
         <div className="first-heading">
           <a href="https://aviyel.com/">
@@ -16,15 +15,13 @@ const Layout = () => {
         </div>
 
         <div className="second-heading">
-          <h1>Wall of Fame</h1>
+          <h1>Request Board</h1>
         </div>
 
         <div className="third-heading">
           <p>Featuring contributions by the Aviyel Community!</p>
         </div>
-        <div className="line-image">
-          <img src={line} alt="" />
-        </div>
+        
       </div>
     </>
   );
