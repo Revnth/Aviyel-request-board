5934c186e84890ccde9bfa25f72ec028a48bd0d2
Devaprasad <devaprasadmohan@gmail.com>
2022-04-06 23:57:20 +0530

    surge testing with project name change

--- src/components/data.js
+++ src/components/data.js
@@ -6,14 +6,14 @@ const Data = [
     
     pic: "https://avatars.githubusercontent.com/u/19822348?v=4",
     url:"https://www.google.com",
-    Name: "Project Name",
+    Name: "Project 1",
     title: "One Line Description",
     desc: "Prerequisites",
   },
   {
     pic: "https://avatars.githubusercontent.com/u/19822348?v=4",
     url:"#",
-    Name: "Project Name",
+    Name: "Project 2",
     title: "One Line Description qwertyuioki gcxvjmln",
     desc: "Prerequisites",
   },
